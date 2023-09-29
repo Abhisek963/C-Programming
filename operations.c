984fc5100366bdf3cbcfd38a66e911ea0c1424bb
Abhisek963 <abhisek.singh@stu.adamasuniversity.ac.in>
2023-09-30 01:07:04 +0530

    operations for(+,-,*,/)

---
+++ operations.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter two number:");
+    scanf("%d%d",&a,&b);
+    printf("sum=%d",a+b);
+    printf("\ndifference=%d",a-b);
+    printf("\nproduct=%d",a*b);
+    printf("\ndivision=%d",a/b);
+}
