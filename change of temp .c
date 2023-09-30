c3b37e7836bff744280e70c7b38b69022b90d886
Abhisek963 <abhisek.singh@stu.adamasuniversity.ac.in>
2023-09-30 23:05:13 +0530

    change of temp

---
+++ change of temp .c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    float c,f;
+    printf("Enter the temperature in celsius:\n");
+    scanf("%f",&c);
+    f=(c*9/5)+32;
+    printf("Temperature in fahrenheit=%f",f);
+
+}
