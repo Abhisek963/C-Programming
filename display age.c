772b6ab5df33c8bac490e9af1780eab0c7ece7e5
Abhisek963 <abhisek.singh@stu.adamasuniversity.ac.in>
2023-09-30 23:25:58 +0530

    display age

---
+++ display age.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    char n,p,e;
+    int b;
+    printf("Enter your name:");
+    scanf("%s",&n);
+    printf("\nEnter your programme:");
+    scanf("%s",&p);
+    printf("\nEnter your email id:");
+    scanf("%s",&e);
+    printf("\nEnter your birth year :");
+    scanf("%d",&b);
+    printf("calculated age=%d",2023-b);
+
+
+}
