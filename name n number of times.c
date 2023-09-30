c8769f625c726ab78d907d33383b09557c7f3b31
Abhisek963 <abhisek.singh@stu.adamasuniversity.ac.in>
2023-09-30 23:49:02 +0530

    by do-while

---
+++ name n number of times.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n,a=1;
+    printf(" Enter the value of n :\n ");
+    scanf("%d",&n);
+    do{
+        printf("my name is Abhisek :\n");
+        a++;
+    }
+    while(a<=n);
+}
