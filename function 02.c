44ea5250885f3c1600436acd28224c537b8fce42
Abhisek963 <abhisek.singh@stu.adamasuniversity.ac.in>
2023-10-09 21:38:10 +0530

    type02

---
+++ function 02.c
@@ -0,0 +1,14 @@
+// Do not except an argument but returns no value
+
+
+#include<stdio.h>
+int add(){
+    int a=10;
+    int b=20;
+    int c=a+b;
+    printf("%d",c);
+}
+int main(){
+   add();
+
+}
