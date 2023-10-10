0f4c24f8127e087e00329935ce949101f39ae127
Abhisek963 <abhisek.singh@stu.adamasuniversity.ac.in>
2023-10-10 22:32:26 +0530

    Display friend name

---
+++ function 07.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int disp(){
+    
+    printf("Abhisek\n");
+    printf("Prakhar\n");
+    printf("Shuhbam\n");
+    printf("Shourav\n");
+    printf("Akash\n");
+}
+int main(){
+    disp();
+}
