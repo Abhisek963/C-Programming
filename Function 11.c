a22bb71dce8f260eeab34c5b010af530ce19a91f
Abhisek963 <abhisek.singh@stu.adamasuniversity.ac.in>
2023-10-12 22:32:48 +0530

    fibonacii pattern using *

---
+++ Function 11.c
@@ -0,0 +1,35 @@
+/*
+ *
+ * *
+ * * *
+ * * * * *
+ */
+//To print pattern using '*' whose logic would be that of fibonacci number's.
+
+
+
+
+#include<stdio.h>
+int fibopattern()
+{
+    int i=1,j,a=0,b=1,sum=0;
+    while(i<=5)
+    {
+        printf("\n");
+        a=b;
+        b=sum;
+        sum= a+b;
+        j=1;
+        while(j<=sum)
+        {
+            printf("*");
+            j++;
+        }
+        i++;
+    }
+}
+int main()
+{
+    fibopattern();
+
+}
