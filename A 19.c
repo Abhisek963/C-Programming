04ce965f0dddadd1b90c4810e9c82bdb08000828
Abhisek963 <abhisek.singh@stu.adamasuniversity.ac.in>
2023-10-16 22:22:14 +0530

    Escape sequences

---
+++ A 19.c
@@ -0,0 +1,21 @@
+//Escape sequnces
+#include<stdio.h>
+int main()
+{
+printf("New line\n");
+printf("Horizontal tab\t");
+printf("Backspace\b");
+printf("Carriage Return\r");
+printf("Audible Bell\a");
+printf("\nPrinting single quotation\'");
+printf("\nPrinting double quotation\" ");
+printf("\nQuestion mark sequence\?");
+printf("\nBack Slash\\");
+printf("\nForm Feed\f");
+printf("Vertical tab\v");
+printf("\nNull value\0");
+printf("\nPrint octal value\n\x6E\x6E\x6E");
+printf("\nPrint hexadecimal value: 89056\x68");
+return 0;
+
+}
