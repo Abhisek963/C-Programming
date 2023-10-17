cbbc570b63c4118d8804fb25812516935c56373d
Abhisek963 <abhisek.singh@stu.adamasuniversity.ac.in>
2023-10-17 19:10:46 +0530

    no.s untill -1 encountered

---
+++ B 01.c
@@ -0,0 +1,25 @@
+//Program to read no.s untill -1 is encountered and also count the no. of negetive, positive and zeros entered
+
+#include<stdio.h>
+int main()
+{
+    int no,neg=0,pos=0,zero=0,a=-1;
+    printf("Enter a number: ");
+    scanf("%d",&no);
+    printf("\nEnter -1 to terminate",a);
+    while(no!=(-1))
+    {
+        if(no<0)
+            neg++;
+        else if(no>0)
+            pos++;
+        else
+            zero++;
+        printf("\nEnter a number: ");
+        scanf("%d",&no);
+    }
+    printf("\nCount of positive numbers entered: %d",pos);
+    printf("\nCount of negetive numbers entered: %d",neg);
+    printf("\nCount of zeros entered: %d",zero);
+    return 0;
+}
