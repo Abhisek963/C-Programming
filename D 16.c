8f7822b814aaf7429a666a67246566fcea9564da
Abhisek963 <abhisek.singh@stu.adamasuniversity.ac.in>
2023-11-07 23:06:18 +0530

    initialization of string

---
+++ D 16.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    char car[5][20]={"Mahindra",
+                     "Audi",
+                     "Lamborghini",
+                     "Hyundai",
+                     "Tata motors"};
+    printf("%S",car[3]);
+}
